c7ee7ab7cd9237fb57738bf87f15ba19081820c6
Scott G. Daniel <scottdaniel25@gmail.com>
2018-04-09 21:29:40 +0000

    WINNING

--- scripts/cfuge_to_genome.py
+++ scripts/cfuge_to_genome.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
-#script that takes a centrifuge report and download genomes and annotations
+'''
+Script that takes a centrifuge report and download genomes and annotations
+'''
+import time
 import sys
 import argparse
 import os
@@ -15,15 +17,11 @@ wc = local['wc']
 
 if __name__ == "__main__":
     parser = \
-    argparse.ArgumentParser(description="Script to just print out names and taxon_ids from a centrifuge report")
+    argparse.ArgumentParser(description="Script that takes a centrifuge report and download genomes and annotations")
     
     parser.add_argument("-r", "--report", action="store", \
             help="Centrifuge report, usually: centrifuge_report.tsv", \
             default='./centrifuge_report.tsv')
-    parser.add_argument("-t", "--taxid", action="store_true", \
-            help="Print only taxids")
-    parser.add_argument("-n", "--name", action="store_true", \
-            help="Print only species names")
     parser.add_argument("-o", "--output", action="store", \
             help="Output directory for genomes and annotations", \
             default='./')
@@ -39,50 +37,70 @@ def filter_report(report):
 
     for row in report.itertuples(index=True, name='Pandas'):
         if getattr(row, 'abundance') > args.min_abundance:
-            if args.taxid is True and args.name is False:
-                print("{:d}".format(getattr(row, 'taxID')))
-                holder.append(getattr(row, 'taxID'))
-            elif args.name is True and args.taxid is False:
-                print("{:s}".format(getattr(row, 'name')))
-                holder.append(getattr(row, 'name'))
-            else:
-                print("{:d}\t{:s}".format(getattr(row,'taxID'),getattr(row,'name')))
-                holder.append(str(getattr(row,'taxID'))+'\t'+str(getattr(row,'name')))
+            print("{:d}".format(getattr(row, 'taxID')))
+            holder.append(getattr(row, 'taxID'))
+    
+    print("After filtering, these are the PATRIC genome id's: {}".format(holder))
     return holder
 
 def download_genomes(filtered_list):
+    
+    #change working directory
+    os.chdir(args.output)
+
     for taxID in filtered_list:
-        chain = p3_all_genomes['-e', 'taxon_id'+','+str(taxID), \
+        try:
+            chain = p3_all_genomes['-e', 'taxon_id'+','+str(taxID), \
                 '-e', 'genome_status,Complete'] | egrep['-v', 'genome']
-        list_of_patricIDs = chain()
-        print('These are all the ids {}'.format(list_of_patricIDs))
+            list_of_patricIDs = chain()
+        except Exception as e:
+            print("Something went wrong with the PATRIC cli. Error: {}".format(e))
+            print("Can not do much without PATRIC cli")
+            print("Contact patricbrc.org")
+            print("Exiting...")
+            sys.exit(1)
 
         for patricID in list_of_patricIDs.split('\n'):
             if patricID != '':
-                print('Getting PATRIC genome_id {} in fasta and Refseq gff formats'.format(patricID))
+                print('Getting PATRIC genome_id {}'.format(patricID))
 
-                wget('ftp://ftp.patricbrc.org/genomes/' + patricID + '/' + \
-                        patricID + '.fna')
-                
-                wget('ftp://ftp.patricbrc.org/genomes/' + patricID + '/' + \
+                try:
+                    print('Getting the genome nucleotide sequence ".fna"')
+                    wget('ftp://ftp.patricbrc.org/genomes/' + patricID + '/' + patricID + '.fna')
+                except Exception as e:
+                    print("Something went wrong with downloading the .fna. Error {}".format(e))
+                    
+                try:
+                    print('Getting the genome RefSeq annotation ".gff"')
+                    wget('ftp://ftp.patricbrc.org/genomes/' + patricID + '/' + \
                         patricID + '.RefSeq.gff')
+                except Exception as e:
+                    print("Something went wrong with downloading the .Refseq.gff Error {}".format(e))
+                
                 #if the RefSeq.gff does not exist or it is too small (usually just means its just a header and nothing else) we get the PATRIC.gff
 
                 if not os.path.isfile(patricID + '.RefSeq.gff'):
-                    wget('ftp://ftp.patricbrc.org/genomes/' + patricID + '/' + \
+                    try:
+                        print('Since there was no RefSeq.gff, trying to get PATRIC.gff for you')
+                        wget('ftp://ftp.patricbrc.org/genomes/' + patricID + '/' + \
                         patricID + '.PATRIC.gff')
+                    except Exception as e:
+                        print("Something went wrong with downloading the .PATRIC.gff Error {}".format(e))
                 else:
                     line_count = int(wc('-l', patricID + '.RefSeq.gff').strip().split()[0])
-                
-                if line_count < 5:
-                    wget('ftp://ftp.patricbrc.org/genomes/' + patricID + '/' + \
-                        patricID + '.PATRIC.gff')
-                    if os.path.isfile(patricID + '.RefSeq.gff'):
-                        os.remove(patricID + '.RefSeq.gff')
+                    if line_count < 5:
+                        try:
+                            print('The RefSeq.gff is less than 5 lines, it is probably bogus. Trying to get PATRIC.gff for you')
+                            wget('ftp://ftp.patricbrc.org/genomes/' + patricID + '/' + \
+                            patricID + '.PATRIC.gff')
+                            os.remove(patricID + '.RefSeq.gff')
+                        except Exception as e:
+                            print("Something went wrong with downloading the .PATRIC.gff Error {}".format(e))
 
-    
+print("Start! {:s}".format(time.ctime()))
+
 report = pd.read_table(args.report,delimiter='\t')
 list_of_taxids_or_names = filter_report(report)
 download_genomes(list_of_taxids_or_names)
-print('Done!')
 
+print("Done! {:s}".format(time.ctime()))
