f1a14822ce19cde6b29430a3560cc1af54b878fb
Scott G. Daniel <scottdaniel25@gmail.com>
2018-04-16 13:48:36 -0500

    updating parameters for deployment

--- stampede/template.sh
+++ stampede/template.sh
@@ -4,5 +4,6 @@ echo "QUERY \"${QUERY}\""
 echo "INDEX  \"${INDEX}\""
 echo "EXCLUDE_TAXIDS \"${EXCLUDE_TAXIDS}\""
 echo "MIN_ABUNDANCE \"${MIN_ABUNDANCE}\""
+echo "FORMAT \"${FORMAT}\""
 
-sh run.sh ${QUERY} ${INDEX} ${EXCLUDE_TAXIDS} ${MIN_ABUNDANCE}
+sh run.sh ${QUERY} ${INDEX} ${EXCLUDE_TAXIDS} ${MIN_ABUNDANCE} ${FORMAT}
